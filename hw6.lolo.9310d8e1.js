7afe13308dc5ce0648ae763c034eeb15152f4ec0
daria4783 <166536416+daria4783@users.noreply.github.com>
2026-05-06 17:42:30 +0200

    Updates

---
+++ hw6.lolo.9310d8e1.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("btn"),t=document.getElementById("nameInput");e.addEventListener("click",()=>{var e,u;let n=(e=t.value||"Гість",`\u{41F}\u{440}\u{438}\u{432}\u{456}\u{442}, ${e}! \u{1F44B}`);u=`${n} | 2+3=5, 2\xd73=6`,document.getElementById("output").textContent=u});
+//# sourceMappingURL=hw6.lolo.9310d8e1.js.map
